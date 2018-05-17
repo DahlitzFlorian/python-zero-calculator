e52a163a7a2457436de385cbe3ac94ecfff92a7d
Florian Dahlitz <f2dahlitz@freenet.de>
2018-05-17 21:38:45 +0200

    implemented a basic stack

---
+++ calculator/helper/stack.py
@@ -0,0 +1,51 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.stack})"
+
+    def __str__(self):
+        return f"{self.stack}"
+
+    def push(self, element):
+        """
+        Adds an element on the top of the stack.
+        """
+        self.stack.append(element)
+
+    def pop(self):
+        """
+        Removes the most recently added element of the stack.
+        """
+        if not self.is_empty():
+            self.stack.pop()
+
+    def peek(self):
+        """
+        Returns the most recently added element of the stack.
+        If the stack is empty, it returns None.
+        """
+        if not self.is_empty():
+            return self.stack[-1]
+        else:
+            return None
+
+    def is_empty(self):
+        """
+        Returns a boolean representing the current status of the stack,
+        whether it's empty or not.
+        """
+        return not len(self.stack)
+
+    @staticmethod
+    def create_from_list(elements: list) -> "Stack":
+        """
+        Gets a list of elements and creates a stack out of it.
+        """
+        s = Stack()
+
+        for element in reversed(elements):
+            s.push(element)
+
+        return s
